3ab502ab5b2f17a325607c7be0cbfb1e56f195da
matapa23 <bofill17@gmail.com>
2023-02-14 18:18:57 +0100

    Creació arxiu css i animacions barra lataral a l'index

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php include "user.php" ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <title>DarniCloud</title>
 </head>
-
 <body>
   <header>
     <nav class="navbar fixed-top" style="background-color: #1299F3; padding:0">
@@ -24,15 +23,14 @@
         <!-- Aquest div es el que done color a la nav bar del deslpagable -->
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Settings</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <!-- //Foto login  -->
-          <div class="card; border:none;" style="display:flex; align-items:center; justify-content:flex-start; gap:0px 20px;">
+          <div class="card; border:none;" id="fotolog">
             <img class="card-img-top" src="./imgs/ahmed.jpg" style="height: 15vh; width: 10vw; border-radius: 50%; border:none;">
             <div class="d-flex flex-column">
-              <label>Ahamde</label>
-              <label>Mostafa</label>
+              <h3>Ahamde</h3>
+              <h4>Mostafa</h4>
             </div>
           </div>
           <!-- ----------------------------------------------------- -->
@@ -40,13 +38,13 @@
             <div class="offcanvas-body">
               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="http://localhost/M09UF2/DarniCloud/overview.php">Overview</a>
+                  <a class="nav-link active barraL" aria-current="page" href="http://localhost/M09UF2/DarniCloud/overview.php">Overview</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" href="#">Download</a>
+                  <a class="nav-link active barraL"  href="#">Download</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" href="#">Preu</a>
+                  <a class="nav-link active barraL" href="#">Preu</a>
                 </li>
                 <li class="nav-item">
                   <form>
@@ -67,8 +65,8 @@
   if (isset($_GET["signin"])) {
     echo '
     <form>
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-image:url(' . 'imgs/nuvols.png' . '); background-repeat: no-repeat; background-size: cover;">
-    <section class="rounded-5 p-5 border border-3 border-dark" style="width: 40%;">
+    <div id="siginlogin">
+    <section id="siginsec" class="rounded-5 p-5 border border-3 border-dark">
     <h1 style="text-align:center">Sign In</h1>
     <div class="mb-3">
     <label class="form-label">Email</label>
@@ -100,7 +98,7 @@
   if (isset($_GET["login"])) {
     echo '
     <form>
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-image:url(' . 'imgs/nuvols.png' . '); background-repeat: no-repeat; background-size: cover;">
+    <div  id="siginlogin">
     <section class="rounded-5 p-5 border border-3 border-dark" style="width: 40%;">
     <h1 style="text-align:center">Log In</h1>
     <div class="mb-3">
